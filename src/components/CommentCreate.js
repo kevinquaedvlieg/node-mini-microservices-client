b6d9281361c54d366e6e1395445546b88337d2b1
kevinquaedvlieg <kevin@kqcreations.com>
2023-01-18 01:01:56 +0100

    Client setup

---
+++ src/components/CommentCreate.js
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import axios from "axios";
+
+function CommentCreate({postId}) {
+    const [content, setContent] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (content === "")
+            return;
+
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        })
+
+        setContent("");
+    }
+
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>New Comment</label>
+                <input
+                    className="form-control"
+                    value={content}
+                    onChange={e => setContent(e.target.value)}
+                />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+}
+
+export default CommentCreate
